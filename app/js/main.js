34ae3171ce7d2a07f90689fc393793a873462d0f
Dishones2 <andrej13524@gmail.com>
2020-08-31 16:35:29 +0600

    project done

---
+++ app/js/main.js
@@ -0,0 +1,26 @@
+$(function(){
+
+    $('.btn-one').on('click', function(){
+        $('.item-one, .circle-one').toggleClass('active')
+    });
+
+    $('.btn-two').on('click', function(){
+        $('.item-two, .circle-two').toggleClass('active')
+    });
+
+    $('.btn-three').on('click', function(){
+        $('.item-three, .circle-three').toggleClass('active')
+    });
+
+    $('.btn-four').on('click', function(){
+        $('.item-four, .circle-four').toggleClass('active')
+    });
+
+    $('.btn-five').on('click', function(){
+        $('.item-five, .circle-five').toggleClass('active')
+    });
+
+
+
+    
+});
